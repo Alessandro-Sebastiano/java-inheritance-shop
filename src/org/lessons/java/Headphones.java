2d436c20c1cf31659f8b0eaba0f58d2543251468
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-03-13 16:01:35 +0100

    class-headphones

---
+++ src/org/lessons/java/Headphones.java
@@ -0,0 +1,38 @@
+package org.lessons.java;
+
+public class Headphones extends Prodotto{
+
+    private String color;
+    private boolean wireless;
+
+    public Headphones(String name, String description, double price, double iva, String color, boolean wireless) {
+        super(name, description, price, iva);
+        this.color = color;
+        this.wireless = wireless;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isWireless() {
+        return wireless;
+    }
+
+    public void setWireless(boolean wireless) {
+        this.wireless = wireless;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "Headphones{" +
+                "color='" + color + '\'' +
+                ", wireless=" + wireless +
+                "} ";
+    }
+}
